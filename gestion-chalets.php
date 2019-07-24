5dc6c8e06d9059aa51a0b7419f54627204e1bff1
reveolte <gregory.barile@gmail.com>
2019-07-24 14:35:48 +0200

    1e commit du plugin

---
+++ gestion-chalets.php
@@ -0,0 +1,106 @@
+<?php
+/**
+ * Plugin Name: Gestion Chalets
+ * Description: Ce plugin ajoute des custom Post Type spécifiques à la gestion des chalets pour la vente et la location. Il ajoute également les templates des ces CPT pour un affichage personnalisé.
+ * Author: Grégory Barile
+ * Version: 0.1
+ */
+//**********************CUSTOM POST TYPE *********************************************
+
+//Chalets à vendre-------------------------------
+
+function wpm_custom_post_type() {
+
+	// Dénominations affichées dans l'administration
+	$labels = array(
+		// Le nom au pluriel
+		'name'                => _x( 'Chalets à vendre', 'Post Type General Name', 'OceanWP'),
+		// Le nom au singulier
+		'singular_name'       => _x( 'Chalet à vendre', 'Post Type Singular Name', 'OceanWP'),
+		// Le libellé affiché dans le menu
+		'menu_name'           => __( 'Chalets Vente', 'OceanWP'),
+		// Les différents libellés de l'administration
+		'all_items'           => __( 'Tous les chalets à vendre', 'OceanWP'),
+		'view_item'           => __( 'Voir les chalets à vendre', 'OceanWP'),
+		'add_new_item'        => __( 'Ajouter un nouveau chalet à vendre', 'OceanWP'),
+		'add_new'             => __( 'Ajouter', 'OceanWP'),
+		'edit_item'           => __( 'Editer le chalet à vendre', 'OceanWP'),
+		'update_item'         => __( 'Modifier le chalet à vendre', 'OceanWP'),
+		'search_items'        => __( 'Rechercher un chalet à vendre', 'OceanWP'),
+		'not_found'           => __( 'Non trouvé', 'OceanWP'),
+		'not_found_in_trash'  => __( 'Non trouvé dans la corbeille', 'OceanWP'),
+	);
+	
+	// Autres options pour chalet à vendre
+	
+	$args = array(
+		'label'               => __( 'Chalets à vendre', 'OceanWP'),
+		'description'         => __( 'Tous les chalets à vendre', 'OceanWP'),
+		'labels'              => $labels,
+        'menu_icon'           => 'dashicons-admin-multisite',
+		'supports'            => array( 'title', ),
+		
+		//Options supplémentaires
+		
+		'show_in_rest' 		  => true,
+		'hierarchical'        => false,
+		'show_ui' 			  => true, 
+		'show_in_menu' 		  => true, 
+		'show_in_nav_menus'	  => true,
+		'show_in_admin_bar'   => true, 
+		'menu_position' 	  => 6, 
+		'can_export' 		  => true,
+		'exclude_from_search' => false,
+    	'publicly_queryable'  => true,
+    	'capability_type' 	  => 'post',
+		'public'              => true,
+		'has_archive'         => true,
+		'rewrite'			  => array( 'slug' => 'chalets-vente'),
+
+	);
+	
+	// Enregistrement CPT et arguments
+	register_post_type( 'chalets_vente', $args );
+
+}
+
+add_action( 'init', 'wpm_custom_post_type', 0 );
+
+
+function prefix_gallery_metabox( $types ) {
+    $types[] = 'chalets_vente';
+    return $types;
+}
+add_filter( 'ocean_gallery_metabox_post_types', 'prefix_gallery_metabox' );
+
+//**********************TEMPLATES *********************************************
+
+//Chalets à vendre-------------------------------
+
+function enqueue_css_chalet(){
+    wp_enqueue_style( 'styles_chalets', plugins_url( 'styles/styles_chalets.css', __FILE__ ));
+}
+
+function enqueue_js_chalet(){
+    wp_enqueue_script( 'js-chalet', plugins_url( 'js/js-chalet.js', __FILE__ ),array(), false, true );
+}
+
+add_action( 'wp_enqueue_scripts', 'enqueue_css_chalet');
+add_action( 'wp_enqueue_scripts', 'enqueue_js_chalet');
+
+
+function load_chalets_vente_template( $single_template ) {
+    global $post;
+
+    if ( 'chalets_vente' === $post->post_type ) {
+        $single_template = dirname( __FILE__ ) . '/single-chalets_vente.php';
+    }
+
+    return $single_template;
+
+}
+
+add_filter( 'single_template', 'load_chalets_vente_template' );
+
+
+
---
+++ single-chalets_vente.php
@@ -0,0 +1,305 @@
+<?php
+
+/*
+ * Template Name: Fiche Chalet
+ * Template Post Type: chalets_vente
+*/
+
+
+function chalets_vente_layout_class( $class ) {
+
+	// Alter your layout
+	if ( is_singular( 'post' ) ) {
+		$class = 'full-width';
+	}
+
+	// Return correct class
+	return $class;
+
+}
+add_filter( 'ocean_post_layout_class', 'chalets_vente_layout_class', 20 );
+
+
+get_header(); ?>
+
+	
+
+<div id="content" class="site-content clr">
+
+	<h2 id="nomChalet"><?php esc_html(the_title());?></h2>
+
+	<div id="sectionSlider">
+
+	</div>
+
+
+		<div id="sliderChalet">
+			<div id="lightFilter">
+			</div>
+			<div id="caracQuartier" class ="caracteristiqueChalet">Quartier : <?php the_field("gc_quartier");?></div>
+			<div id="caracPrix" class ="caracteristiqueChalet">Prix : <?php the_field("gc_prix");?> €</div>
+			<div id="caracSuperficie" class ="caracteristiqueChalet">Superficie : <?php the_field("gc_superficie");?> m²</div>
+
+			<div id="sliderThumbnails">
+				<p id="leftArrow" class = "sliderArrow"><</p>
+
+			<?php 
+
+				$arrayImage = oceanwp_get_gallery_ids();
+
+				foreach ($arrayImage as $key => $id) {
+					echo '<div class="chaletThumbnail" style="background-image:url('.wp_get_attachment_url($id).')"><div class="whiteFilter"></div></div>';
+
+				}
+
+
+				?>
+				<p id="rightArrow" class = "sliderArrow">></p>
+
+			</div>
+
+		</div>
+
+
+
+	<section id = "sectionCaracteristiques">
+		<div id="descriptif">
+			<h3>Caractéristiques du bien</h3>
+			<p><?php the_field("gc_description");?>
+			<div id="features">
+				<div id="disposition">
+
+					<div class="caseDispo">						
+						<img class="iconeBien" src="http://15.188.4.60/wp-content/uploads/2019/07/surface-bleu.png">
+						<p><?php echo the_field("gc_superficie").' m²'; ?></p>
+					</div>
+
+					<div class="caseDispo">						
+						<img class="iconeBien" src="http://15.188.4.60/wp-content/uploads/2019/07/plan-bleu.png">
+						<p>
+							<?php
+							//vérif singulier / pluriel
+							$nbPieces = get_field('gc_pieces');
+							if ($nbPieces>1){
+								$motPiece=' pièces';
+							}
+							else{
+								$motPiece=' pièce';
+							}
+							echo the_field("gc_pieces").$motPiece;?>
+						</p>
+					</div>
+
+					<div class="caseDispo">						
+						<img class="iconeBien" src="http://15.188.4.60/wp-content/uploads/2019/07/chambre-bleu.png">
+						<p>
+							<?php
+							//vérif singulier / pluriel
+							$nbChambres = get_field('gc_chambres');
+							if ($nbChambres>1){
+								$motChambres=' chambres';
+							}
+							else{
+								$motChambres=' chambre';
+							}
+							echo the_field("gc_chambres").$motChambres;?>
+						</p>
+					</div>
+
+					<div class="caseDispo">						
+						<img class="iconeBien" src="http://15.188.4.60/wp-content/uploads/2019/07/sdb-bleu.png">
+						<p>
+							<?php
+							//vérif singulier / pluriel
+							$nbSDB = get_field('gc_salles_de_bain');
+							if ($nbSDB>1){
+								$motSDB=' salles de bain';
+							}
+							else{
+								$motSDB=' salle de bain';
+							}
+							echo the_field("gc_salles_de_bain").$motSDB;?>
+						</p>
+					</div>
+
+					<div class="caseDispo">						
+						<img class="iconeBien" src="http://15.188.4.60/wp-content/uploads/2019/07/stairs-bleu.png">
+						<p>
+							<?php
+							//vérif singulier / pluriel
+							$nbEtages = get_field('gc_etages');
+							if($nbEtages==0){
+								echo 'Plain-pied';
+							}
+							elseif($nbEtages>1){
+								$motEtage=' étages';
+								echo the_field("gc_etages").$motEtage;
+							}
+							else{
+								$motEtage=' étage';
+								echo the_field("gc_etages").$motEtage;
+							}
+							?>
+						</p>
+					</div>
+
+					
+
+				</div>
+				<div id="amenagements">
+					<div id="interieur">
+						<div class="bloc-amenagement">						
+								<p class="titreFeature">Aménagements intérieurs</p>
+								<?php 
+									$arrInterieurs=get_field('gc_amenagements_interieurs');
+									if($arrInterieurs!=""){
+										foreach ($arrInterieurs as $key => $amenagement) {
+											switch ($amenagement) {
+												case 'Cuisine équipée':
+												echo '<div class="amenagement-interieur"><img alt="Cuisine équipée" src="http://15.188.4.60/wp-content/uploads/2019/07/hotte-bleu.png"><p>Cuisine équipée</p></div>';
+												break;
+
+											case 'Cuisine américaine':
+												echo '<div class="amenagement-interieur"><img alt="Cuisine américaine" src="http://15.188.4.60/wp-content/uploads/2019/07/cuisineAmericaine-bleu.png"><p>Cuisine américaine</p></div>';
+												break;
+
+											case 'Véranda':
+												echo '<div class="amenagement-interieur"><img alt="Véranda" src="http://15.188.4.60/wp-content/uploads/2019/07/veranda-bleu.png"><p>Véranda</p></div>';
+												break;
+
+											case 'Dressing':
+												echo '<div class="amenagement-interieur"><img alt="Dressing" src="http://15.188.4.60/wp-content/uploads/2019/07/cintre-bleu.png"><p>Dressing</p></div>';
+												break;
+
+											case 'Cheminée':
+												echo '<div class="amenagement-interieur"><img alt="Cheminée" src="http://15.188.4.60/wp-content/uploads/2019/07/cheminee-bleu.png"><p>Cheminée</p></div>';
+												break;
+
+											default:
+												echo '<div class="amenagement-interieur"><img alt="'.$amenagement.'" src="http://15.188.4.60/wp-content/uploads/2019/07/like-bleu.png"><p>'.$amenagement.'</p></div>';
+												
+												break;
+											}
+										}
+									}
+									
+								?>
+								
+							</div>
+					</div>
+
+					<div id="exterieur">
+						<div class="bloc-amenagement">	
+							<p class="titreFeature">Aménagements extérieurs</p>
+							<?php 
+								$arrExterieurs=get_field('gc_amenagements_exterieurs');
+								if($arrExterieurs!=""){
+									foreach ($arrExterieurs as $key => $amenagement) {
+										switch ($amenagement) {
+											case 'Garage fermé':
+											echo '<div class="amenagement-exterieur"><img alt="Garage fermé" src="http://15.188.4.60/wp-content/uploads/2019/07/garage-bleu.png"><p>Garage fermé</p></div>';
+											break;
+
+											case 'Garage avec prise électrique':
+											echo '<div class="amenagement-exterieur"><img alt="Garage avec prise électrique" src="http://15.188.4.60/wp-content/uploads/2019/07/electricite-bleu.png"><p>Garage avec prise électrique</p></div>';
+											break;
+
+											case 'Coin barbecue':
+											echo '<div class="amenagement-exterieur"><img alt="Coin barbecue" src="http://15.188.4.60/wp-content/uploads/2019/07/BBQ-bleu.png"><p>Coin barbecue</p></div>';
+											break;
+
+											case 'Jardin':
+											echo '<div class="amenagement-exterieur"><img alt="Jardin" src="http://15.188.4.60/wp-content/uploads/2019/07/jardin-bleu.png"><p>Jardin</p></div>';
+											break;
+
+											default:
+												echo '<div class="amenagement-exterieur"><img alt="'.$amenagement.'" src="http://15.188.4.60/wp-content/uploads/2019/07/like-bleu.png"><p>'.$amenagement.'</p></div>';
+												
+											break;
+										}
+									}
+								}
+
+							?>
+						</div>
+					</div>	
+				</div>
+					
+				</div>
+				
+		</div>
+		<div id="tableauServices">
+			<h4>La touche de l'Architecte</h4>
+
+			<div id="listeServices">
+				<?php $arrCheckBox = get_field('gc_services');
+
+				if(!empty($arrCheckBox)){
+
+					foreach ($arrCheckBox as $key => $service) {
+						switch ($service) {
+							case 'Sauna':
+								echo '<div class="service"><img alt="sauna" src="http://15.188.4.60/wp-content/uploads/2019/07/sauna-blanc.png"><p>Sauna</p></div>';
+								break;
+
+							case 'Hammam':
+								echo '<div class="service"><img alt="Hammam" src="http://15.188.4.60/wp-content/uploads/2019/07/hammam-blanc.png"><p>Hammam</p></div>';
+								break;
+
+							case 'Piscine':
+								echo '<div class="service"><img alt="Piscine" src="http://15.188.4.60/wp-content/uploads/2019/07/piscine-blanc.png"><p>Piscine</p></div>';
+								break;
+
+							case 'Jacuzzi intérieur':
+								echo '<div class="service"><img alt="Jacuzzi" src="http://15.188.4.60/wp-content/uploads/2019/07/jacuzzi-blanc.png"><p>Jacuzzi</p></div>';
+								break;
+
+							case 'Jacuzzi extérieur':
+								echo '<div class="service"><img alt="Jacuzzi extérieur" src="http://15.188.4.60/wp-content/uploads/2019/07/jacuzzi-ext-blanc.png"><p>Jacuzzi extérieur</p></div>';
+								break;
+
+							case 'Local à ski':
+								echo '<div class="service"><img alt="Local à ski" src="http://15.188.4.60/wp-content/uploads/2019/07/ski-blanc.png"><p>Local à ski</p></div>';
+								break;
+
+							case 'Salle de cinéma':
+								echo '<div class="service"><img alt="Salle de cinéma" src="http://15.188.4.60/wp-content/uploads/2019/07/cine-blanc.png"><p>Salle de cinéma</p></div>';
+								break;
+
+							case 'Vue panoramique':
+								echo '<div class="service"><img alt="Vue panoramique" src="http://15.188.4.60/wp-content/uploads/2019/07/panorama-blanc.png"><p>Vue panoramique</p></div>';
+								break;
+
+							default:
+								echo '<div class="service"><img alt="Vue panoramique" src="http://15.188.4.60/wp-content/uploads/2019/07/vue.png"><p>Vue panoramique</p></div>';
+								
+								break;
+						}
+					}
+
+				}
+
+				if(get_field('gc_touche')!=""){
+					$touche=get_field('gc_touche');
+					echo '<p id="toucheArchi">"'.$touche.'"</p>';
+				}
+
+				?>
+			</div>
+		</div>
+
+		<div id="fixedBtnWrapper">
+			<div id="btnContact" class="button-wrapper">
+				<a href="">
+					<span>CONTACTER L'AGENCE</span>
+				</a>
+			</div>
+			
+		</div>
+
+	</section>
+
+</div><!-- #content -->
+
+
+<?php get_footer(); ?>
