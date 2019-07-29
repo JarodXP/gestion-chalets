b1bbd3eb4873750ca116e234d7727d3e6079c89a
reveolte <gregory.barile@gmail.com>
2019-07-29 14:37:27 +0200

    Front JS form validation

--- js/js-chalet.js
+++ js/js-chalet.js
@@ -126,6 +126,50 @@ function addEventsArrowCTA(){
 }
 
 //---------------FORMULAIRE DE CONTACT -------------
+//Securisation
+
+
+function verifChamps(){
+	var email=document.querySelector("#formMail>input");
+	var nom=document.querySelector("#formName>input");
+	var prenom=document.querySelector("#formFirstName>input");
+	var message=document.querySelector("#formMessage>input");
+	var verifMail = /^[a-z0-9._-]+@[a-z0-9._-]{2,}\.[a-z]{2,4}$/;	
+	var verifNom = /^[a-zA-ZáàâäãåçéèêëíìîïñóòôöõúùûüýÿæœÁÀÂÄÃÅÇÉÈÊËÍÌÎÏÑÓÒÔÖÕÚÙÛÜÝŸÆŒ-]{1,50}$/;
+
+	if(verifMail.exec(email.value)==null){
+		alert("Votre email est incorrect");
+		return false;
+	}
+	else if(verifNom.exec(nom.value)==null){
+		alert("Votre nom est incorrect");
+		return false;
+	}
+	else if(verifNom.exec(prenom.value)==null){
+		alert("Votre prénom est incorrect");
+		return false;
+	}
+	else if(message==""||message.length<10){
+		alert("Votre message est trop court");
+		return false;
+	}
+	else{
+		return true;
+	}
+
+}
+
+function addEventSubmit(){
+	var formulaire=document.getElementById("formulaire");
+	formulaire.addEventListener("submit", function(e){
+		if(verifChamps()==false){
+
+			e.preventDefault();
+		}
+	})
+}
+
+
 function addEventBtnCTA(){
 	var contactForm=document.getElementById("contactForm");
 	var btnCTA = document.getElementById('fixedBtnWrapper');
@@ -141,5 +185,6 @@ function addEventBtnCTA(){
 
 addEventsArrowCTA();
 addEventBtnCTA();
+addEventSubmit();
 
 
--- templates/formulaire-contact.php
+++ templates/formulaire-contact.php
@@ -1,23 +1,24 @@
-<form action="traitement-formulaire.php" method="post">
+<form id="formulaire" action="http://chaletsetcaviar.jarod-xp.com/wp-content/plugins/gestion-chalets/templates/traitement-formulaire.php" method="post">
 
 	<div id="formName" class="form-input">
 		<label for="contact_last_name">Nom</label>
-		<input type="text" name="contact_last_name">
+		<input type="text" name="contact_last_name" maxlength="50">
+		<input type="hidden" name="email" maxlength="50" required>
 	</div>
 
-	<div id="formFirstMail" class="form-input">
+	<div id="formFirstName" class="form-input">
 		<label for="contact_first_name">Prénom</label>
-		<input type="text" name="contact_first_name">
+		<input type="text" name="contact_first_name" maxlength="50" required>
 	</div>
 
 	<div id="formMail" class="form-input">
 		<label for="contact_mail">Email</label>
-		<input type="email" name="contact_mail">
+		<input type="email" name="contact_mail" maxlength="50" required>
 	</div>
 
 	<div id="formMessage" class="form-input">
 		<label for="contact_message">Message</label>
-		<input type="textarea" name="contact_message">
+		<input type="textarea" name="contact_message" maxlength="400" required>
 	</div>
 	
     <button id="formSubmit" type="submit">Envoyer le message</button>
@@ -30,12 +31,12 @@
     		<div id="blocDates">
     			<div id="formDateDu" class="form-input">
 					<label for="date_du">Date d'arrivée</label>
-					<input type="date" name="date_du">
+					<input type="date" name="date_du" maxlength="50">
 				</div>
 
 				<div id="formDateAu" class="form-input">
 					<label for="date_au">Date de départ</label>
-					<input type="date" name="date_au">
+					<input type="date" name="date_au" maxlength="50">
 				</div>
     			
     		</div>
